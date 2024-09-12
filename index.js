879e00ddbccc765caf7dd92746d6429df2906707
Nishanth R <69259993+nishanth123kgr@users.noreply.github.com>
2024-09-12 23:30:50 +0530

    v3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var colors = ['green', 'red', 'yellow', 'blue'], gamePattern = [], userClickedPattern = [], level = 0, x = 0, speed = 500;
+var colors = ['green', 'red', 'yellow', 'blue'], gamePattern = [], userClickedPattern = [], level = 0, x = 0, speed = 500, reverseMode = false;
 
 $(document).keypress(() => {
-    if (level == 0)
+    if (level == 0) {
+        reverseMode = false;
         nextSequence();
+    }
 });
 
 $('.btn').click((event) => {
@@ -12,9 +14,10 @@ $('.btn').click((event) => {
 });
 
 function verify(id) {
-    if ((colors[gamePattern[x]] === id) && (x < gamePattern.length)) {
+    let correctId = reverseMode ? colors[gamePattern[gamePattern.length - 1 - x]] : colors[gamePattern[x]];
+
+    if ((correctId === id) && (x < gamePattern.length)) {
         x++;
-        console.log('matches');
         press(id);
         if (x === gamePattern.length) {
             x = 0;
@@ -41,26 +44,37 @@ function gameOver() {
 
 function nextSequence() {
     setTimeout(() => {
-        randomColor = Math.floor(Math.random() * 4);
+        let randomColor = Math.floor(Math.random() * 4);
         level += 1;
-        
-        // Aggressive speed increase after level 6
-        if (level > 6 && speed > 200) {
-            speed -= 50; // Speed increases faster after level 6
-        } else if (level > 10 && speed > 100) {
-            speed -= 100; // After level 10, speed increases drastically
+
+        // Aggressively increase speed after level 6
+        if (level > 6 && speed > 150) {
+            speed -= 50;
+        } else if (level > 10 && speed > 50) {
+            speed -= 50; // Extremely fast after level 10
         }
         
-        $('#level-title').text('Level ' + level);
+        // Reverse mode after level 10
+        if (level > 10) {
+            reverseMode = true;
+            $('#level-title').text('Level ' + level + ' (REVERSE MODE)');
+        } else {
+            $('#level-title').text('Level ' + level);
+        }
 
-        // Introduce distractions after level 8
+        // Shuffle pattern slightly after level 7
+        if (level > 7 && Math.random() > 0.5) {
+            shufflePattern();
+        }
+
+        // Introduce multiple distractions starting from level 5
+        if (level > 5) {
+            randomDistractions();
+        }
+
+        // Add random rapid blinking from level 8
         if (level > 8) {
-            randomDistraction();
-        }
-
-        // Introduce unpredictability after level 10
-        if (level > 10 && Math.random() > 0.5) {
-            randomColor = Math.floor(Math.random() * 4); // Unpredictable behavior after level 10
+            blinkColors();
         }
 
         gamePattern.push(randomColor);
@@ -69,7 +83,7 @@ function nextSequence() {
 }
 
 function press(id) {
-    $('.' + id).fadeOut(100).fadeIn(100);
+    $('.' + id).fadeOut(50).fadeIn(50);
     new Audio('./sounds/' + id + '.mp3').play();
 }
 
@@ -77,13 +91,38 @@ function animatePress(currentColor) {
     $('.' + currentColor).addClass('pressed');
     setTimeout(() => {
         $('.' + currentColor).removeClass('pressed');
-    }, 200);
+    }, 100);
 }
 
-function randomDistraction() {
-    setTimeout(() => {
-        var distractionColor = colors[Math.floor(Math.random() * 4)];
-        press(distractionColor);
-        console.log("Distraction: " + distractionColor);
-    }, Math.random() * 1000); // Distraction occurs at random times
+function randomDistractions() {
+    let distractionCount = Math.floor(Math.random() * 3) + 1; // 1 to 3 distractions
+    for (let i = 0; i < distractionCount; i++) {
+        setTimeout(() => {
+            let distractionColor = colors[Math.floor(Math.random() * 4)];
+            press(distractionColor);
+            console.log("Distraction: " + distractionColor);
+        }, Math.random() * 500); // Distractions occur at random times
+    }
+}
+
+function shufflePattern() {
+    // Randomly shuffle a few elements in the existing pattern to confuse the player
+    if (gamePattern.length > 3) {
+        let shuffleIndex1 = Math.floor(Math.random() * gamePattern.length);
+        let shuffleIndex2 = Math.floor(Math.random() * gamePattern.length);
+        let temp = gamePattern[shuffleIndex1];
+        gamePattern[shuffleIndex1] = gamePattern[shuffleIndex2];
+        gamePattern[shuffleIndex2] = temp;
+    }
+}
+
+function blinkColors() {
+    let blinkCount = Math.floor(Math.random() * 3) + 1; // 1 to 3 random blinks
+    for (let i = 0; i < blinkCount; i++) {
+        setTimeout(() => {
+            let blinkColor = colors[Math.floor(Math.random() * 4)];
+            $('.' + blinkColor).fadeOut(20).fadeIn(20);
+            console.log("Blink: " + blinkColor);
+        }, Math.random() * 500); // Blinks occur at random times
+    }
 }
